c46ef413039cbbb103c165e3ed385aa490f15bc6
MatthewNobes <mattnobes.personal@protonmail.com>
2019-06-21 22:00:48 +0100

    Add project files.

---
+++ NoughtsAndCrosses/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace NoughtsAndCrosses
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::NoughtsAndCrosses.Properties.Resources.NoughtsAndCrosses;
+            this.ClientSize = new System.Drawing.Size(1921, 1024);
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.Name = "MainForm";
+            this.Text = "Noughts And Crosses";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
