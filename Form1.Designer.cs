b268ad80e927fbc57cd8fca338967940c29e0770
MatthewNobes <mattnobes.personal@protonmail.com>
2020-05-12 10:50:02 +0100

    Upload new version
    (not functional)

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Noughts_and_Crosses
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic8 = new System.Windows.Forms.PictureBox();
+            this.pic6 = new System.Windows.Forms.PictureBox();
+            this.pic9 = new System.Windows.Forms.PictureBox();
+            this.pic5 = new System.Windows.Forms.PictureBox();
+            this.pic7 = new System.Windows.Forms.PictureBox();
+            this.pic4 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic8
+            // 
+            this.pic8.BackColor = System.Drawing.SystemColors.Control;
+            this.pic8.Location = new System.Drawing.Point(419, 531);
+            this.pic8.Name = "pic8";
+            this.pic8.Size = new System.Drawing.Size(400, 250);
+            this.pic8.TabIndex = 17;
+            this.pic8.TabStop = false;
+            // 
+            // pic6
+            // 
+            this.pic6.BackColor = System.Drawing.SystemColors.Control;
+            this.pic6.Location = new System.Drawing.Point(831, 269);
+            this.pic6.Name = "pic6";
+            this.pic6.Size = new System.Drawing.Size(400, 250);
+            this.pic6.TabIndex = 16;
+            this.pic6.TabStop = false;
+            // 
+            // pic9
+            // 
+            this.pic9.BackColor = System.Drawing.SystemColors.Control;
+            this.pic9.Location = new System.Drawing.Point(831, 531);
+            this.pic9.Name = "pic9";
+            this.pic9.Size = new System.Drawing.Size(400, 250);
+            this.pic9.TabIndex = 15;
+            this.pic9.TabStop = false;
+            // 
+            // pic5
+            // 
+            this.pic5.BackColor = System.Drawing.SystemColors.Control;
+            this.pic5.Location = new System.Drawing.Point(419, 269);
+            this.pic5.Name = "pic5";
+            this.pic5.Size = new System.Drawing.Size(400, 250);
+            this.pic5.TabIndex = 14;
+            this.pic5.TabStop = false;
+            // 
+            // pic7
+            // 
+            this.pic7.BackColor = System.Drawing.SystemColors.Control;
+            this.pic7.Location = new System.Drawing.Point(7, 531);
+            this.pic7.Name = "pic7";
+            this.pic7.Size = new System.Drawing.Size(400, 250);
+            this.pic7.TabIndex = 13;
+            this.pic7.TabStop = false;
+            // 
+            // pic4
+            // 
+            this.pic4.BackColor = System.Drawing.SystemColors.Control;
+            this.pic4.Location = new System.Drawing.Point(7, 269);
+            this.pic4.Name = "pic4";
+            this.pic4.Size = new System.Drawing.Size(400, 250);
+            this.pic4.TabIndex = 12;
+            this.pic4.TabStop = false;
+            // 
+            // pic3
+            // 
+            this.pic3.BackColor = System.Drawing.SystemColors.Control;
+            this.pic3.Location = new System.Drawing.Point(831, 7);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(400, 250);
+            this.pic3.TabIndex = 11;
+            this.pic3.TabStop = false;
+            // 
+            // pic2
+            // 
+            this.pic2.BackColor = System.Drawing.SystemColors.Control;
+            this.pic2.Location = new System.Drawing.Point(419, 13);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(400, 250);
+            this.pic2.TabIndex = 10;
+            this.pic2.TabStop = false;
+            // 
+            // pic1
+            // 
+            this.pic1.BackColor = System.Drawing.SystemColors.Control;
+            this.pic1.Location = new System.Drawing.Point(7, 13);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(400, 250);
+            this.pic1.TabIndex = 9;
+            this.pic1.TabStop = false;
+            this.pic1.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1245, 790);
+            this.Controls.Add(this.pic8);
+            this.Controls.Add(this.pic6);
+            this.Controls.Add(this.pic9);
+            this.Controls.Add(this.pic5);
+            this.Controls.Add(this.pic7);
+            this.Controls.Add(this.pic4);
+            this.Controls.Add(this.pic3);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic8;
+        private System.Windows.Forms.PictureBox pic6;
+        private System.Windows.Forms.PictureBox pic9;
+        private System.Windows.Forms.PictureBox pic5;
+        private System.Windows.Forms.PictureBox pic7;
+        private System.Windows.Forms.PictureBox pic4;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Noughts_and_Crosses
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public partial class MainForm : Form
+        {
+            public bool turnTimer = false;
+
+            public bool winIsLost = false;
+
+            public bool oneFull = false;
+            public bool twoFull = false;
+            public bool threeFull = false;
+            public bool fourFull = false;
+            public bool fiveFull = false;
+            public bool sixFull = false;
+            public bool sevenFull = false;
+            public bool eightFull = false;
+            public bool nineFull = false;
+
+            public string one;
+            public string two;
+            public string three;
+            public string four;
+            public string five;
+            public string six;
+            public string seven;
+            public string eight;
+            public string nine;
+
+            public MainForm()
+            {
+                InitializeComponent();
+
+
+
+            }
+
+
+            public static class Globals
+            {
+                // global int
+                public static bool team;
+            }
+
+            private void pic5_Click(object sender, EventArgs e)
+            {
+
+                if (Globals.team == true)
+                {
+                    pic5.Image = Properties.Resources.Nought;
+                    Globals.team = false;
+                }
+                else
+                {
+                    pic5.Image = Properties.Resources.cross;
+                    Globals.team = true;
+                }
+            }
+
+            private void pic1_Click(object sender, EventArgs e)
+            {
+                if (Globals.team == true)
+                {
+                    pic5.Image = Properties.Resources.Nought;
+                    Globals.team = false;
+                }
+                else
+                {
+                    pic5.Image = Properties.Resources.cross;
+                    Globals.team = true;
+                }
+            }
+
+            private void pic2_Click(object sender, EventArgs e)
+            {
+                if (Globals.team == true)
+                {
+                    pic5.Image = Properties.Resources.Nought;
+                    Globals.team = false;
+                }
+                else
+                {
+                    pic5.Image = Properties.Resources.cross;
+                    Globals.team = true;
+                }
+            }
+
+            private void pic3_Click(object sender, EventArgs e)
+            {
+                if (Globals.team == true)
+                {
+                    pic5.Image = Properties.Resources.Nought;
+                    Globals.team = false;
+                }
+                else
+                {
+                    pic5.Image = Properties.Resources.cross;
+                    Globals.team = true;
+                }
+            }
+
+            private void pic4_Click(object sender, EventArgs e)
+            {
+                if (Globals.team == true)
+                {
+                    pic5.Image = Properties.Resources.Nought;
+                    Globals.team = false;
+                }
+                else
+                {
+                    pic5.Image = Properties.Resources.cross;
+                    Globals.team = true;
+                }
+            }
+
+            private void pic6_Click(object sender, EventArgs e)
+            {
+                if (Globals.team == true)
+                {
+                    pic5.Image = Properties.Resources.Nought;
+                    Globals.team = false;
+                }
+                else
+                {
+                    pic5.Image = Properties.Resources.cross;
+                    Globals.team = true;
+                }
+            }
+
+            private void pic7_Click(object sender, EventArgs e)
+            {
+                if (Globals.team == true)
+                {
+                    pic5.Image = Properties.Resources.Nought;
+                    Globals.team = false;
+                }
+                else
+                {
+                    pic5.Image = Properties.Resources.cross;
+                    Globals.team = true;
+                }
+            }
+
+            private void pic8_Click(object sender, EventArgs e)
+            {
+                if (Globals.team == true)
+                {
+                    pic5.Image = Properties.Resources.Nought;
+                    Globals.team = false;
+                }
+                else
+                {
+                    pic5.Image = Properties.Resources.cross;
+                    Globals.team = true;
+                }
+            }
+
+            private void pic9_Click(object sender, EventArgs e)
+            {
+                if (Globals.team == true)
+                {
+                    pic5.Image = Properties.Resources.Nought;
+                    Globals.team = false;
+                }
+                else
+                {
+                    pic5.Image = Properties.Resources.cross;
+                    Globals.team = true;
+                }
+            }
+        }
+
+        private void pic1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+}
